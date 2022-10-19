eb9f7db2cb4ab6d9aa83d778369d2615bb7e1423
Ontaria Woods <ontaria.woods@gmail.com>
2022-10-19 11:26:16 -0400

    commit tree f5209e827bb8e023da1d7866dab88bb14c834518

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+var upperCaseCustomerName = function () {
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'not Ontaria'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Jarius'
+    return leastFavoriteCustomer
+}
